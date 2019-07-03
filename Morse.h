05692f2e90e8a7af8b0c5829149644a9c445c466
zrjassy <47413198+zrjassy@users.noreply.github.com>
2019-07-03 21:25:13 +0800

    Add files via upload

---
+++ Morse.h
@@ -0,0 +1,14 @@
+#ifndef _MORSE_H
+#define _MORSE_H
+class Morse
+{
+  public:
+    Morse(int pin);
+    void dot();
+    void dash();
+    void c_space();
+    void w_space();
+  private:
+    int _pin;
+};
+#endif /*_MORSE_H*/
